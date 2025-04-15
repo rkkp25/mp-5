f14338a6c449c8b6240ea0b98c97cf6705c1af67
rkkp25 <rkkp@bu.edu>
2025-04-15 13:27:38 -0400

    trying to get the link

--- components/AliasPreview.tsx
+++ components/AliasPreview.tsx
@@ -1,10 +1,15 @@
 import { AliasProps } from "@/types";
+import { redirect } from 'next/navigation'
+
 
 
 export default function AliasPreview({alias}: {alias: AliasProps }) {
+    function goToLink(alias: string) {
+        redirect("https://mp-5-pi.vercel.app/{alias}")
+    }
     return (
         <div className="bg-rose-100 rounded-xl p-4 mt-2 w-96">
-            <a className="font-bold text-xl" target="_blank" href="https://cs391-url-shortener.vercel.app/{alias.alias}">https://cs391-url-shortener.vercel.app/{alias.alias}</a>
+            <button className="font-bold text-xl" onClick={() => goToLink(alias.alias)}>https://mp-5-pi.vercel.app/{alias.alias}</button>
         </div>
     );
 }
